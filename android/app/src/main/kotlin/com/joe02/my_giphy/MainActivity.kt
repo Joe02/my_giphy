776e81b6818ef9dbf46024b3abcd1af85474d4a4
joe02 <joe02araujo@gmail.com>
2020-09-16 11:03:41 -0300

    First commit.

---
+++ android/app/src/main/kotlin/com/joe02/my_giphy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.joe02.my_giphy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/giphy_home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class GiphyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => GiphyHomePageState();
+}
+
+class GiphyHomePageState extends State<GiphyHomePage> {
+  String _search;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
